7e53ec98e8127a78578f8c28527fdaaed22df986
zhaqsylyq <zhaksylyk.chalgimbayev@nu.edu.kz>
2025-01-20 20:07:40 +0500

    backend creation and security configuration

---
+++ whatsapp/src/main/java/com/zhaqsylyq/whatsapp/WhatsappApiApplication.java
@@ -0,0 +1,13 @@
+package com.zhaqsylyq.whatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappApiApplication.class, args);
+	}
+
+}
---
+++ whatsapp/src/test/java/com/zhaqsylyq/whatsapp/WhatsappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaqsylyq.whatsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
